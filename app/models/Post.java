995e3358d355dc3e920f797c22cbd6bbd540dcfc
IgorAnohhin <i.anohhin@gmail.com>
2012-09-24 20:43:34 +0300

    First model

---
+++ app/models/Post.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Igor
+ * Date: 24.09.12
+ * Time: 18:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class Post {
+}
