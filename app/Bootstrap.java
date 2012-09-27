183b83589b6809356aa738a4a40edeb2e2b92faf
IgorAnohhin <i.anohhin@gmail.com>
2012-09-27 23:33:51 +0300

    Validation and basic CRUD

---
+++ app/Bootstrap.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Igor
+ * Date: 25.09.12
+ * Time: 19:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class Bootstrap {
+}
---
+++ app/controllers/Posts.java
@@ -0,0 +1,11 @@
+package controllers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Igor
+ * Date: 27.09.12
+ * Time: 23:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class Posts {
+}
