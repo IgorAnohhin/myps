402755453b378cee15379e65ef23a7dc1e6689ad
IgorAnohhin <i.anohhin@gmail.com>
2012-09-29 02:11:05 +0300

    Admin panel generated, Post form edited, wysiwyg added

---
+++ app/controllers/Users.java
@@ -0,0 +1,11 @@
+package controllers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Igor
+ * Date: 28.09.12
+ * Time: 23:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class Users {
+}
