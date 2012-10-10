19999097b9df7c5c35c58b2d3599f5b00080074f
IgorAnohhin <i.anohhin@gmail.com>
2012-10-10 21:53:59 +0300

    Created Menu model

---
+++ app/models/Menu.java
@@ -0,0 +1,12 @@
+package models;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Igor
+ * Date: 7.10.12
+ * Time: 17:45
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Menu {
+    FRONT, WORKS, SKILLS, INTERESTS
+}
