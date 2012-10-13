66a7f67bfdc8fcd5b09eabf541b865593dd64636
IgorAnohhin <i.anohhin@gmail.com>
2012-10-13 18:01:39 +0300

    sql

--- db/evolutions/1.sql
+++ db/evolutions/1.sql
@@ -1,17 +1,16 @@
 # Users schema
-
+ 
 # --- !Ups
-
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) DEFAULT NULL,
-  `fullName` varchar(255) DEFAULT NULL,
-  `isAdmin` bit(1) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+ 
+CREATE TABLE User (
+    id bigint(20) NOT NULL AUTO_INCREMENT,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    fullname varchar(255) NOT NULL,
+    isAdmin boolean NOT NULL,
+    PRIMARY KEY (id)
 );
-
+ 
 # --- !Downs
-
-DROP TABLE `user`;
+ 
+DROP TABLE User;
